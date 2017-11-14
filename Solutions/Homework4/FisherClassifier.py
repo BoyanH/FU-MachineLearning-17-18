ba8d0b5536794937fff7eb06de6c0d24f76893eb
Boyan Hristov <bhristov96@gmail.com>
2017-11-15 00:20:05 +0100

    added plot

--- Solutions/Homework4/FisherClassifier.py
+++ Solutions/Homework4/FisherClassifier.py
@@ -25,10 +25,46 @@ class FisherClassifier(Classifier):
         return split_X
 
     @staticmethod
-    def get_density_function(center, v):
+    def get_density_function(center, covariance):
         return lambda x: math.e ** (
-            (-1/2) * ((x - center) / v) ** 2
-        ) / (v * math.sqrt((2*math.pi)))
+            (-1/2) * ((x - center) / covariance) ** 2
+        ) / (covariance * math.sqrt((2*math.pi)))
+
+    @staticmethod
+    def get_covariance_for_projected(points, center):
+        vectorized_sq_distances_sum = np.vectorize(lambda x, m: (x - m)**2)
+        square_distances_sum = np.sum(vectorized_sq_distances_sum(points, center))
+        return math.sqrt(square_distances_sum / len(points))
+
+    def plot_class(self, points, center):
+        projected_center = self.project_point(center)
+        # project_points = np.vectorize(lambda x:  self.project_point(x), signature='(n)->(n)')
+        projected_points = list(map(lambda x: self.project_point(x), points))
+        covariance = FisherClassifier.get_covariance_for_projected(projected_points, projected_center)
+        density_a = FisherClassifier.get_density_function(projected_center, covariance)
+        plot_distance = 5000
+        y_of_plot = [density_a(float(x) / 100) for x in range(-plot_distance, plot_distance)]
+        x_to_plot = [float(x) / 100 for x in range(-plot_distance, plot_distance)]
+        first_decent = None
+        last_decent = None
+
+        for idx, y in enumerate(y_of_plot):
+            if y > 0.01 and first_decent is None:
+                first_decent = idx
+            elif y <= 0.01 and first_decent is not None:
+                last_decent = idx
+                break
+
+        beauty_margin = (last_decent - first_decent)
+        start = int(first_decent - beauty_margin/2)
+        end = int(last_decent + beauty_margin/2)
+
+        plt.plot(x_to_plot[start:end], y_of_plot[start:end])
+
+    def plot_probability_distribution(self, center_a, center_b, points_a, points_b):
+        self.plot_class(points_a, center_a)
+        self.plot_class(points_b, center_b)
+        plt.show()
 
     def project_point(self, x):
         return x.dot(self.alpha)
@@ -48,24 +84,7 @@ class FisherClassifier(Classifier):
         # on the 1 dimensional space. This one is the projected point between the 2 centers
         self.threshold = self.project_point((center_a + center_b) / 2)
 
-        projected_center_a = self.project_point(center_a)
-        projected_center_b = self.project_point(center_b)
-        covariance = 1
-        plot_distance = int((max(projected_center_a, projected_center_b) -
-                         min(projected_center_a, projected_center_b)) * 2 * 100)
-
-        density_a = FisherClassifier.get_density_function(projected_center_a, covariance)
-        density_b = FisherClassifier.get_density_function(projected_center_b, covariance)
-
-        plt.plot([float(x)/100 for x in range(-plot_distance, plot_distance)],
-                 [density_a(float(x)/100) for x in range(-plot_distance, plot_distance)])
-
-        plt.plot([float(x)/100 for x in range(-plot_distance, plot_distance)],
-                 [density_b(float(x)/100) for x in range(-plot_distance, plot_distance)])
-        plt.show()
-
-
-        # print(self.project_point(cov_mat_a))
+        self.plot_probability_distribution(center_a, center_b, X_a, X_b)
 
     def predict(self, X):
         return list(map(lambda x: self.predict_single(x), X))
@@ -95,7 +114,7 @@ class FisherClassifier(Classifier):
 # print('Best score for seed={}: {}'.format(best_seed, max_score))
 # print('Worst score for seed{}: {}'.format(worst_seed, min_score))
 
-X_train, X_test, y_train, y_test = get_data_set(int(random() * 1000))
+X_train, X_test, y_train, y_test = get_data_set(879)
 classifier = FisherClassifier(X_train, y_train)
 score = classifier.score(X_test, y_test)
 print('Score: {}'.format(classifier.score(X_test, y_test)))
