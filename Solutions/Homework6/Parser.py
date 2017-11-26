0ed538bbedad76bc0176301bd1846ab87962a5fb
Boyan Hristov <bhristov96@gmail.com>
2017-11-26 15:12:33 +0100

    refined solution, everything working; maximum of 500 iterations without convergence getting any better

--- Solutions/Homework6/Parser.py
+++ Solutions/Homework6/Parser.py
@@ -11,7 +11,7 @@ def parse_data():
 
 
 def get_points_and_labels_from_data(data):
-    points = data[:,:-1].astype('float')
+    points = np.array(data[:,:-1], dtype=np.float64)
     labels = data[:,-1]
 
     return points, labels
@@ -27,7 +27,7 @@ def get_data_set(seed):
     data = parse_data()
     X, y = get_points_and_labels_from_data(data)
     # for determined results we use a seed for random_state, so that data is always split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8, test_size=0.2,
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.9, test_size=0.1,
                                                         random_state=seed)
 
     return X_train, X_test, y_train, y_test
--- Solutions/Homework6/PerceptronClassifier.py
+++ Solutions/Homework6/PerceptronClassifier.py
@@ -9,28 +9,29 @@ from scipy.stats import multivariate_normal
 
 classes_in_data_set = ['Iris-virginica', 'Iris-setosa', 'Iris-versicolor']
 infinity = float('inf')
-learning_rate = 0.00001
+learning_rate = 0.0001
 
 class PerceptronClassifier(Classifier):
     def __init__(self, X, y, class_a, class_b):
-        ones = np.ones((len(X), 1), dtype=float)
+        ones = np.ones((len(X), 1), dtype=np.float64)
         X_normalized = np.append(ones, X, axis=1)
         y_normalized = np.vectorize(lambda x: -1 if x == class_a else 1)(y)
+        np.random.seed(8)
         self.w = X_normalized[np.random.randint(0, X_normalized.shape[0], 1)][0]
-        self.fit(X_normalized, y_normalized)
         self.class_a = class_a
         self.class_b = class_b
+        self.fit(X_normalized, y_normalized)
 
     def fit(self, X, y):
         t = 0
         least_error = None
         current_error = None
         best_w = self.w
+        worse_iterations = 0
 
-        while least_error is None or current_error < least_error:  #  < least_error:
-            if current_error is not None and (least_error is None or current_error < least_error):
-                least_error = current_error
-                best_w = self.w
+        while worse_iterations < 500 and (current_error is None or current_error > 0):  #  < least_error:
+            if least_error is not None and current_error > least_error:
+                worse_iterations += 1
 
             current_error = 0
 
@@ -42,25 +43,33 @@ class PerceptronClassifier(Classifier):
             # learning rate is something commonly used in this algorithm, in the lecture we learned
             # a simplified version where the learning rate is 1
             for x, yi in zip(X, y):
-                error = (self.predict_single_normalized(x) - yi)*x
-                w_new = self.w - learning_rate*error
+                error = ((self.predict_single_normalized(x) - yi)/2)
+                w_new = self.w - learning_rate*error*x
                 current_error += PerceptronClassifier.get_error(w_new, self.w)
                 self.w = w_new
+
+            if current_error is not None and (least_error is None or current_error < least_error):
+                least_error = current_error
+                best_w = self.w
+
+        for x, yi in zip(X, y):
+            error = (self.predict_single_normalized(x) - yi)
+            assert(error == 0 or current_error > 0)
+
+        print('Least error for {} vs {}: {}deg'.format(self.class_a, self.class_b, least_error))
 
         self.w = best_w / np.linalg.norm(best_w)
 
     @staticmethod
     def get_error(w_new, w):
-        # err in degrees rotation is
-        # math.acos(w_new.dot(w)/(np.linalg.norm(w)*np.linalg.norm(w_new)))
-        # though we need it just for comparison, so the dot multiplication is enough
-        return w_new.dot(w)
+        # err in degrees rotation
+        return math.acos(np.clip((w_new / np.linalg.norm(w_new)).dot(w / np.linalg.norm(w)), -1.0, 1.0))
 
     def project_point(self, x):
         return x.dot(self.w / np.linalg.norm(self.w))
 
     def predict_single_normalized(self, x):
-        return 1 if self.project_point(x) > 0 else 0
+        return 1 if self.project_point(x) > 0 else -1
 
     def predict_single(self, x):
         x_normalized = np.append(np.array([1]), x)
@@ -79,19 +88,19 @@ X_vi_se_train, y_vi_se_train = extract_classes_from_data_set(X_train, y_train, c
 X_vi_se_test, y_vi_se_test = extract_classes_from_data_set(X_test, y_test, classes_in_data_set[:-1])
 pc = PerceptronClassifier(X_vi_se_train, y_vi_se_train, classes_in_data_set[0], classes_in_data_set[1])
 score = pc.score(X_vi_se_test, y_vi_se_test)
-print(score)
+print('Score {} vs {}: {}'.format(classes_in_data_set[0], classes_in_data_set[1], score))
 
 
 X_ve_se_train, y_ve_se_train = extract_classes_from_data_set(X_train, y_train, classes_in_data_set[1:])
 X_ve_se_test, y_ve_se_test = extract_classes_from_data_set(X_test, y_test, classes_in_data_set[1:])
 pc = PerceptronClassifier(X_ve_se_train, y_ve_se_train, classes_in_data_set[1], classes_in_data_set[2])
 score2 = pc.score(X_ve_se_test, y_ve_se_test)
-print(score2)
+print('Score {} vs {}: {}'.format(classes_in_data_set[1], classes_in_data_set[2], score2))
 
 X_vi_ve_train, y_vi_ve_train = extract_classes_from_data_set(X_train, y_train, [classes_in_data_set[0],
                                                              classes_in_data_set[2]])
 X_vi_ve_test, y_vi_ve_test = extract_classes_from_data_set(X_test, y_test, [classes_in_data_set[0],
                                                                             classes_in_data_set[2]])
-pc = PerceptronClassifier(X_ve_se_train, y_ve_se_train, classes_in_data_set[0], classes_in_data_set[2])
+pc = PerceptronClassifier(X_vi_ve_train, y_vi_ve_train, classes_in_data_set[0], classes_in_data_set[2])
 score3 = pc.score(X_vi_ve_test, y_vi_ve_test)
-print(score3)
+print('Score {} vs {}: {}'.format(classes_in_data_set[0], classes_in_data_set[2], score3))
