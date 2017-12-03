86540265962fe07c51bb0fb115cd1c89e444db7b
Boyan Hristov <bhristov96@gmail.com>
2017-12-03 16:23:47 +0100

    logistic regression classifier now fully working

--- Solutions/Homework7/LogisticRegression.py
+++ Solutions/Homework7/LogisticRegression.py
@@ -2,26 +2,85 @@ from Classifier import Classifier
 from Parser import get_data_set
 import numpy as np
 import math
+from matplotlib import pyplot as plt
+from sklearn.linear_model import LogisticRegression as LRSKL
+import os
 
 
 class LogisticRegression(Classifier):
-    def __init__(self, X_train, y_train, learn_rate=5e-4):
+    # def __init__(self, X_train, y_train, learn_rate=1e-3):
+    def __init__(self, X_train, y_train, learn_rate=1e-3):
         self.beta = None
+        self.transformation_vector = None
         self.learn_rate = learn_rate
-        self.fit(np.array(X_train), y_train)
+        X, y = self.transform(X_train, y_train)
+        self.fit(X, y)
 
-    def fit(self, X_train, y_train):
-        features_len = len(X_train[0])
-        self.beta = np.matrix(np.zeros(features_len), dtype=np.float64).reshape(features_len, 1)
+    @staticmethod
+    def sigmoid(weighted):
+        return 1 / (1 + np.exp(-weighted))
 
-        for i in range(1000):
-            self.beta = self.beta + self.learn_rate * \
-                                    self.get_integrated_error(X_train, y_train).T
+    def transform(self, X, y):
+
+        # feature_means = X.sum(0) / len(X[0])
+        # X_t = X.T
+        # variance = [((X_t[i] - feature_means[i])**2).sum() for i in range(len(X_t))]
+
+        # numpy does it more effectively and normalized
+        self.transformation_vector = np.var(X, axis=0)
+        X = X / self.transformation_vector
+
+        # add ones
+        ones = np.ones((len(X), 1), dtype=np.float64)
+        X = np.append(ones, X, axis=1)
+
+        return X, y
+
+    def fit(self, X, y):
+        features_len = len(X[0])
+        self.beta = np.zeros(features_len, dtype=np.float64)
+        log_error_over_time = []
+
+        last_log_error = float('inf')
+        current_learning_rate = self.learn_rate
+
+        for i in range(40000):
+            weighted = X.dot(self.beta)
+            probabilities = LogisticRegression.sigmoid(weighted)
+            directions = y - probabilities
+            gradient = X.T.dot(directions)
+
+            # Bold driver technique
+            # If error was actually larger (overshooting) use previous weight vector
+            # and decrease learning rate by 50%; otherwise increase learn rate by 5%
+
+            # self.beta = self.beta + (self.learn_rate / (2**(i/5000)))*gradient
+            self.learn_rate = self.learn_rate / 2
+            self.beta = self.beta + current_learning_rate*gradient
 
             if i % 100 == 0:
-                print(self.log_likelihood(X_train, y_train))
+                current_log_error = abs(self.get_log_likelihood(X, y))
+
+                if current_log_error < last_log_error:
+                    current_learning_rate += current_learning_rate * 0.0003  # increase by 0.03%
+                else:
+                    self.beta = last_beta
+                    current_learning_rate -= current_learning_rate * 0.03 # decrease by 3%
+
+                last_log_error = current_log_error
+                last_beta = np.copy(self.beta)
+                log_error_over_time.append(current_log_error)
+
+        plt.ylabel('Absolute Log Likelihood')
+        plt.xlabel('Iterations')
+        plt.plot([i for i in range(0, 40000, 100)], log_error_over_time)
+        plt.savefig(os.path.join(os.path.dirname(__file__), 'll_over_time.png'))
 
     def predict(self, X):
+        X = X / self.transformation_vector
+        # add ones
+        ones = np.ones((len(X), 1), dtype=np.float64)
+        X = np.append(ones, X, axis=1)
         return list(map(lambda x: self.predict_single(x), X))
 
     def get_integrated_error(self, X, y):
@@ -43,12 +102,17 @@ class LogisticRegression(Classifier):
     def predict_single(self, x):
         return 1 if self.get_probability(x, 1) > self.get_probability(x, 0) else 0
 
-    def log_likelihood(self, X, y):
-        scores = np.dot(X, self.beta)
-        ll = np.sum(y * scores - np.log(1 + np.exp(scores)))
-        return ll
+    def get_log_likelihood(self, X, y):
+        weighted = X.dot(self.beta)
+        return np.sum(y * weighted - np.log(1 + np.exp(weighted)))
 
 X_train, X_test, y_train, y_test = get_data_set(1)
 lr = LogisticRegression(X_train, y_train)
 score = lr.score(X_test, y_test)
 print('Score: {}'.format(score))
+
+sklearn_lr = LRSKL()
+sklearn_lr.fit(X_train, y_train)
+predictions = sklearn_lr.predict(X_test)
+score_sklearn = np.mean(predictions == y_test)
+print('Score from sklearn: {}'.format(score_sklearn))
