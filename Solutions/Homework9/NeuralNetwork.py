5b82470f3b9a43844834f7f499b91331223fc2b3
Boyan Hristov <bhristov96@gmail.com>
2017-12-17 05:23:22 +0100

    fixed errors in implementation; score: 87% ;/ could get much better, perhaps more fiddling with parameters or adaptive learning_rate needed

--- Solutions/Homework9/NeuralNetwork.py
+++ Solutions/Homework9/NeuralNetwork.py
@@ -6,7 +6,7 @@ from Parser import get_data_set
 
 
 class NeuralNetwork(Classifier):
-    def __init__(self, layers=None, max_iterations=3000, learning_rate=0.0001, online=False):
+    def __init__(self, layers=None, max_iterations=1000, learning_rate=0.01, online=False):
         self.data_normalizer = DataNormalizer()
         self.W_ext = None
         self.max_iterations = max_iterations
@@ -26,29 +26,25 @@ class NeuralNetwork(Classifier):
             self.W_ext = np.array([np.ones((len(X[0]) if i == 0 else self.layers[i-1], self.layers[i]))
                                    for i in self.layers])
         else:
-            dims = 30
-            self.W_ext = np.array([np.ones((len(X[0]), dims)), np.ones((dims + 1, 10))])
+            dims = 30  # 30
+            self.W_ext = np.array([np.random.randn(len(X[0]) + 1, dims), np.random.randn(dims + 1, 10)])
 
-        for i in range(self.max_iterations):
-            W_ext_deltas = None
-            for idx, x in enumerate(X):
-                O_s = self.get_O_s(x)
-                D_s = [np.diag((o * (1-o)).flatten()) for o in O_s[1:]]
-                e = (O_s[-1] - NeuralNetwork.get_single_label_expectation(y[idx])).flatten()
+        batch_size = 1000
+        for batch_start in range(0, len(X), batch_size):
+            Xb = X[batch_start:batch_start + batch_size + 1]
+            yb = y[batch_start:batch_start + batch_size + 1]
+            for it in range(self.max_iterations):
+                O_s = self.get_O_s(Xb)
+                D_s = [(o * (1.0-o)) for o in O_s[1:]]
+                e = (O_s[-1] - NeuralNetwork.get_label_expectation(yb))
                 der_e_s = self.get_der_e_s(D_s, e)
                 delta_W_ext = self.get_delta_W_ext(der_e_s, O_s)
 
-                if W_ext_deltas is None:
-                    W_ext_deltas = delta_W_ext
-                else:
-                    for i, delta in enumerate(delta_W_ext):
-                        W_ext_deltas[i] = W_ext_deltas[i] + delta
-
-            for i, delta in enumerate(W_ext_deltas):
-                self.W_ext[i] += delta
-
-            print(self.score(X_train, y_train))
-            # print(self.W_ext[0])
+                for i, delta in enumerate(delta_W_ext):
+                    self.W_ext[i] += delta
+                # print(self.predict(Xb))
+                if it % 100 == 0:
+                    print('Score: {}; batch: {}'.format(self.score(X_train, y_train), batch_start / batch_size))
 
 
     @staticmethod
@@ -69,10 +65,10 @@ class NeuralNetwork(Classifier):
     def sigmoid(x):
         return 1 / (1 + np.exp(-x))
 
-    def get_O_s(self, x):
-        O_s = [x[1:].reshape(1, x.shape[0]-1)]
+    def get_O_s(self, X):
+        O_s = [X]
         for i in range(self.k_layers):
-            O_i_minus_1 = np.c_[np.array([1]), O_s[-1]] # extend to get o(i-1)^
+            O_i_minus_1 = np.c_[np.ones(len(X)), O_s[-1]] # extend to get o(i-1)^
             O_i = NeuralNetwork.sigmoid(O_i_minus_1.dot(self.W_ext[i]))
             O_s.append(O_i)
 
@@ -80,13 +76,12 @@ class NeuralNetwork(Classifier):
 
     def get_der_e_s(self, D_s, e):
         W = [w[1:] for w in self.W_ext]
-        der_e_l = D_s[-1].dot(e)
-        der_e_l = der_e_l.reshape(der_e_l.shape[0], 1)
+        der_e_l = e * D_s[-1]
         der_e_s = [der_e_l]
 
         for i in range(self.k_layers - 1):
-            der_e_i =  D_s[-i - 2].dot(W[-i - 1]).dot(der_e_s[0])
-            der_e_i = der_e_i.reshape(der_e_i.shape[0], 1)
+            der_e_i =  (der_e_s[0]).dot(W[-i - 1].T)* D_s[-i - 2]
+            der_e_i = np.matrix(der_e_i)
             der_e_s = [der_e_i] + der_e_s
 
         return der_e_s
@@ -95,26 +90,23 @@ class NeuralNetwork(Classifier):
         delta_W_ext = []
 
         for i in range(self.k_layers):
-            delta_W_i = (-der_e_s[i].dot(O_s[i])).T
-            delta_W_i = np.vstack((delta_W_i, np.ones(delta_W_i.shape[1])))
+            o_i_ext = np.c_[np.ones(len(O_s[i])), O_s[i]]
+            delta_W_i = o_i_ext.T.dot(der_e_s[i])
             delta_W_ext.append(delta_W_i)
 
-        return np.array(delta_W_ext) * self.learning_rate
+        return np.array(delta_W_ext) * -self.learning_rate
 
     def predict(self, X):
         X = self.data_normalizer.transform(X)
         return self.predict_(X)
 
     def predict_(self, X):
-        return [self.predict_single(x) for x in X]
-
-    def predict_single(self, x):
-        O_s = self.get_O_s(x)
+        O_s = self.get_O_s(X)
         results = O_s[-1]
-        return results.argmax()
+        return results.argmax(1)
 
 
 X_train, X_test, y_train, y_test = get_data_set('digits.data')
 nn = NeuralNetwork()
 nn.fit(X_train, y_train)
-print(nn.score)
+print(nn.score(X_test, y_test))
